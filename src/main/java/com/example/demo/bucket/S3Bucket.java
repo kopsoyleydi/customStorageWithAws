ea9272dd2cc67a5422555ec2b7b199b821888654
kopsoyleydy <30636@iitu.edu.kz>
2023-08-23 10:30:14 +0600

    server with aws cloud, beginning step

---
+++ src/main/java/com/example/demo/bucket/S3Bucket.java
@@ -0,0 +1,18 @@
+package com.example.demo.bucket;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "aws.s3.buckets")
+public class S3Bucket {
+	private String customer;
+
+	public String getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+}
---
+++ src/main/java/com/example/demo/restControllers/MainController.java
@@ -0,0 +1,54 @@
+package com.example.demo.restControllers;
+
+
+import com.example.demo.bucket.S3Bucket;
+import com.example.demo.bucket.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("home/")
+public class MainController {
+
+	private static Logger logger = Logger.getLogger(MainController.class.getName());
+	@Autowired
+	private S3Service s3Service;
+
+	@Autowired
+	private S3Bucket s3Buckets;
+
+	String arr = "";
+
+	@PostMapping("/upload")
+	public void putFile(@RequestBody MultipartFile file){
+			String profileImageId = UUID.randomUUID().toString();
+			arr = profileImageId;
+			try {
+				s3Service.putObject(
+						"bekscloud",
+						"profile-images/%s".formatted(profileImageId),
+						file.getBytes()
+				);
+				logger.info("success");
+			} catch (IOException e) {
+				throw new RuntimeException("failed to upload profile image", e);
+			}
+	}
+
+	@GetMapping("/getfile")
+	public byte[] getFileFromAws(){
+		try {
+			return s3Service.getObject("bekscloud", "profile-images/%s".formatted(arr));
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}

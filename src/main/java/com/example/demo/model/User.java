3d023b1c4b66b0194ae40163cdcee1b9e7272c86
kopsoyleydy <30636@iitu.edu.kz>
2023-08-25 09:15:49 +0600

    fix

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,17 @@
 package com.example.demo.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
+
+	private Long id;
+
+	private String name;
+
+	private String surname;
+
 
 }
